c7b534ab82d59a3de05e942405fa4e6a07624372
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2018-03-04 17:45:30 -0600

    updating with work done from laptop

--- piano/pianoscript.js
+++ piano/pianoscript.js
@@ -3,15 +3,19 @@ var count2=0;
 function changing(){
     if(count==0)
         {
-            document.getElementById("image").src="images/topnote.PNG";
+            document.getElementById("image").src="images/linenote.PNG";
             count=1;
         }
     else
         {
-            document.getElementById("image").src="images/nonote.PNG";
+            document.getElementById("image").src="images/emptyline.PNG";
             count=0;
         }
 }
 function changingimage2(){
-    document.getElementById("image").src="images/midnote.PNG";
+    document.getElementById("image2").src="images/linenote.PNG";
+}
+function changeboth(){
+    document.getElementById("image").src="images/midnotetop.PNG";
+    document.getElementById("image2") src="images/midnotebot.PNG";
 }
