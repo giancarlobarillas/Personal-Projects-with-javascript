3af11dba4d5b9c974d94acbbfc164c91f3abc900
giancarlobarillas <gbarilla@hawk.iit.edu>
2018-03-29 00:49:02 -0500

    adding edits to piano

--- piano/pianoscript.js
+++ piano/pianoscript.js
@@ -2,15 +2,31 @@ var count=0;
 var count2=0;
 var count3=0;
 var counter=0;
+var pianoMidArray=[{name:'F Note',image:'images/emptyline.PNG',pressed:0},
+                   {name:'E Note',image:'images/emptyline.PNG',pressed:0},
+                   {name:'D Note',image:'images/emptyline.PNG',pressed:0},
+                   {name:'C Note',image:'images/emptyline.PNG',pressed:0},
+                   {name:'B Note',image:'images/emptyline.PNG',pressed:0},
+                   {name:'A Note',image:'images/emptyline.PNG',pressed:0},
+                   {name:'G Note',image:'images/emptyline.PNG',pressed:0},
+                   {name:'F Note',image:'images/emptyline.PNG',pressed:0},
+                   {name:'E Note',image:'images/emptyline.PNG',pressed:0}];
+
 function changing(){
     if(count==0)
         {
-            document.getElementById("image").src="images/linenote.PNG";
-            count=1;
+            pianoMidArray[0].image='images/linenote.PNG';
+            var list=document.getElementById("myList");
+            list.removeChild(list.childNodes[0]);
+            var node = document.createElement("LI");
+            var divadd = document.createElement("div");
+            divadd.innerHTML='<img src="'+pianoMidArray[0].image+'">';
+            node.appendChild(divadd);
+            document.getElementById("myList").appendChild(node); 
         }
     else
         {
-            document.getElementById("image").src="images/emptyline.PNG";
+            pianoMidArray[0].image='images/emptyline.PNG';
             count=0;
         }
 }
@@ -40,13 +56,12 @@ function changemid(){
         count3=0;
     }
 }
-function myFunction() {
+function createpiano() {
     for(var i=0;i<9;i++){
-    var node = document.createElement("LI");
-    var divadd = document.createElement("div");
-    counter++;
-    divadd.innerHTML=counter;
-    node.appendChild(divadd);
-    document.getElementById("myList").appendChild(node);   
+        var node = document.createElement("LI");
+        var divadd = document.createElement("div");
+        divadd.innerHTML='<img src="'+pianoMidArray[i].image+'">';
+        node.appendChild(divadd);
+        document.getElementById("myList").appendChild(node);   
     }
 }
