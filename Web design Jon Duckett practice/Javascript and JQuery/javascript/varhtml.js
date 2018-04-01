c57e28797466f47700894d4f3f5796411bfea153
giancarlobarillas <gbarilla@hawk.iit.edu>
2018-03-31 23:01:27 -0500

    add two new pages for project
    add string javascript and quotes javascript

---
+++ Web design Jon Duckett practice/Javascript and JQuery/javascript/varhtml.js
@@ -0,0 +1,9 @@
+var title;
+var goback;
+title="Hello Person";
+goback='<a href="../index.html"> Back to Main Page</a>'
+
+var elTitle=document.getElementById('title');
+elTitle.innerHTML=title;
+var elBack=document.getElementById('note');
+elBack.innerHTML=goback;
---
+++ Web design Jon Duckett practice/Javascript and JQuery/javascript/varstring.js
@@ -0,0 +1,9 @@
+var username;
+var message;
+username="Giancarlo";
+message="See the rest of the pratices";
+
+var elName=document.getElementById('name');
+elName.textContent=username;
+var elMessage=document.getElementById('note');
+elMessage.textContent=message;
