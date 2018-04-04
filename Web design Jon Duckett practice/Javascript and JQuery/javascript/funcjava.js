3dcc78d176ec47e6a9d67268b3ea9db81ef10d02
giancarlobarillas <gbarilla@hawk.iit.edu>
2018-04-04 01:16:23 -0500

    add new chapter 2 sections on functions

---
+++ Web design Jon Duckett practice/Javascript and JQuery/javascript/funcjava.js
@@ -0,0 +1,6 @@
+var message="Sign up for free! Find the roommate you deserve!";
+function updateMessage(){
+    var el=document.getElementById('message');
+    el.textContent=message;
+}
+updateMessage();
